7e6bfb3534af7e0254238159c8793d38cd8f625c
Farman Ahmed <36335149+farman99ahmed@users.noreply.github.com>
2021-08-29 21:17:28 +0530

    Create A2Q3.java

---
+++ A2Q3.java
@@ -0,0 +1,49 @@
+class fetchData extends Thread {
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        System.out.println("fetchData executed.");
+    }
+    public static void main(String args[]) {
+        fetchData fd = new fetchData();
+        fd.start();
+    }
+}
+
+class processData implements Runnable {
+    public void run() {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+
+        }
+        System.out.println("processData executed.");
+    }
+    public static void main(String args[]) {
+        processData pd = new processData();
+        Thread th1 = new Thread(pd);
+        th1.start();
+    }
+}
+
+public class A2Q3 {
+    public static void main(String args[]) {
+        fetchData fd = new fetchData();
+        processData pd = new processData();
+        Thread th2 = new Thread(pd);
+        th2.setPriority(Thread.MIN_PRIORITY);
+        fd.setPriority(Thread.MAX_PRIORITY);
+        try {
+            fd.start();
+            fd.join();
+            th2.start();
+        } catch(InterruptedException e) {
+             System.out.println(e);    
+        }
+
+    }
+}
